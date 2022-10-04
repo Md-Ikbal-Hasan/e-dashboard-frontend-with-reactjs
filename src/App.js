2615fab8ec2d33918cb134a80a08fb47ff291722
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-04 10:42:00 +0600

    navbar footer and signup page design done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// import component
+import Nav from './components/Nav'
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<h1>Its Products listing component</h1>} ></Route>
+          <Route path="/add" element={<h1>Its add Products  component</h1>} ></Route>
+          <Route path="/update" element={<h1>Its update Products  component</h1>} ></Route>
+          <Route path="/logout" element={<h1>Its logout   component</h1>} ></Route>
+          <Route path="/profile" element={<h1>Its profile  component</h1>} ></Route>
+          <Route path="/signup" element={<SignUp />} ></Route>
+        </Routes>
+      </BrowserRouter>
+
+
+      <Footer />
+
+    </div >
   );
 }
 
