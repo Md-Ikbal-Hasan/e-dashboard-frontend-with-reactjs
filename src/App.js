e879e43a0ff255dda93a4cba726acbf63a77ff95
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-05 13:11:12 +0600

    product update added and product crud done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PrivateComponent from './components/PrivateComponent';
 import Login from './components/Login';
 import AddProduct from './components/AddProduct';
 import ProductList from './components/ProductList';
+import Updateproduct from './components/UpdateProduct';
 
 
 
@@ -24,7 +25,7 @@ function App() {
           <Route element={<PrivateComponent />} >
             <Route path="/" element={<ProductList />} ></Route>
             <Route path="/add" element={<AddProduct />} ></Route>
-            <Route path="/update" element={<h1>Its update Products  component</h1>} ></Route>
+            <Route path="/update/:id" element={<Updateproduct />} ></Route>
             <Route path="/logout" element={<h1>Its logout   component</h1>} ></Route>
             <Route path="/profile" element={<h1>Its profile  component</h1>} ></Route>
 
--- src/components/UpdateProduct.js
+++ src/components/UpdateProduct.js
@@ -1,77 +1,78 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const AddProduct = () => {
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom'
+const Updateproduct = () => {
 
     const [name, setName] = useState("");
     const [price, setPrice] = useState("");
     const [category, setCategory] = useState("");
     const [company, setCompany] = useState("");
-    const [error, setError] = useState(false);
+    const params = useParams();
     const navigate = useNavigate();
 
+    useEffect(() => {
+        getProductDetails();
 
-    const addProduct = async () => {
+    }, [])
 
-        if (!name || !price || !category || !company) {
-            setError(true);
-            return false;
-        }
+    const getProductDetails = async () => {
+        // console.log(params);
+        let result = await fetch(`http://localhost:5000/product/${params.id}`);
+        result = await result.json();
+        setName(result.name);
+        setPrice(result.price);
+        setCategory(result.category);
+        setCompany(result.company);
+    }
 
-        const userId = JSON.parse(localStorage.getItem("user"))._id;
-        // console.log(name, price, category, company, userId);
 
-        let result = await fetch("http://localhost:5000/add-product", {
-            method: "post",
-            body: JSON.stringify({ name, price, category, company, userId }),
+    const updateProduct = async () => {
+        // console.log(name, price, category, company);
+        let result = await fetch(`http://localhost:5000/product/${params.id}`, {
+            method: 'put',
+            body: JSON.stringify({ name, price, category, company }),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
 
         result = await result.json();
-        // console.log(result);
-        navigate("/");
-    }
+        navigate("/")
 
+    }
 
 
 
     return (
         <div className="product">
-            <h1>Add product</h1>
+            <h1>Update product</h1>
             <input type="text" className='inputBox' name=""
                 placeholder='Enter Product Name'
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
-            {error && !name && <small className='invalid-input'>Enter Valid Name</small>}
 
             <input type="text" className='inputBox' name=""
                 placeholder='Enter Product Price'
                 value={price}
                 onChange={(e) => setPrice(e.target.value)}
             />
-            {error && !price && <small className='invalid-input'>Enter Valid Price</small>}
 
             <input type="text" className='inputBox' name=""
                 placeholder='Enter Product Category'
                 value={category}
                 onChange={(e) => setCategory(e.target.value)}
             />
-            {error && !category && <small className='invalid-input'>Enter Valid Category Name</small>}
 
             <input type="text" className='inputBox' name=""
                 placeholder='Enter Product Company'
                 value={company}
                 onChange={(e) => setCompany(e.target.value)}
             />
-            {error && !company && <small className='invalid-input'>Enter Valid Company Name</small>}
 
-            <button onClick={addProduct} className='appButton'>Add Product</button>
+            <button onClick={updateProduct} className='appButton'>Update Product</button>
 
         </div>
     );
 };
 
-export default AddProduct;
+export default Updateproduct;
